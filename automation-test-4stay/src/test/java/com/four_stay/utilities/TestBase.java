e7819f54d2a38cd293e38649855c2426aedc3072
parhat93 <parhat93@gmail.com>
2018-03-14 22:21:43 -0400

    4stay project fisrt face beta

---
+++ automation-test-4stay/src/test/java/com/four_stay/utilities/TestBase.java
@@ -0,0 +1,30 @@
+package com.four_stay.utilities;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+
+protected WebDriver driver;
+	
+	@BeforeClass(alwaysRun=true)
+	public  void setUp() {
+		driver =Driver.getDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(ConfigurationReader.getProperty("url"));
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void tearDown() {
+		 Driver.closeDriver();
+	}
+}
