6f5e67efa51bed1d8fa2536246a7dba09f9045f9
parhat93 <parhat93@gmail.com>
2018-03-17 18:32:37 -0400

    parhat's testcases

--- automation-test-4stay/src/test/java/com/four_stay/pages/F_Parhay_16_19/HomePage.java
+++ automation-test-4stay/src/test/java/com/four_stay/pages/F_Parhay_16_19/HomePage.java
@@ -1,6 +1,8 @@
 package com.four_stay.pages.F_Parhay_16_19;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.four_stay.utilities.Driver;
@@ -19,6 +21,40 @@ public class HomePage {
 	}
 	
 	
-	//	@FindBy(className="billLabel")
-	//	public WebElement billLabel;
+	@FindBy(xpath="//div[@class='form-group']/input")
+	public WebElement searchBox;
+	
+	
+	@FindBy(xpath="//*[@class='btn search-btn']")
+	public WebElement searchButton;
+	
+	@FindBy(xpath="//*[@class='tooltip-inner']")
+	public WebElement AfterclicktheButton;
+	
+	public void clickSearchBox() {
+		searchBox.click();
+	
+	}
+	public void sendKeyToSearchBox() {
+		searchBox.sendKeys("Jones Branch Dr, Tysons , VA 22102, USA");
+		
+	}
+	
+	
+	
+	public void ClickSearchButton() {
+		searchButton.click();
+	}
+	
+	
+	public boolean isAt() {
+		return driver.getTitle().equals("Room rental, roommate finder, off-campus housing, homestay | 4stay");
+	}
+	
+	public boolean isAtUrl() {
+		return driver.getCurrentUrl().equals("https://4stay.com/");
+	}
+	
+	
+	
 }
--- automation-test-4stay/src/test/java/com/four_stay/test/F_Parhat_16_19/SignUpTest.java
+++ automation-test-4stay/src/test/java/com/four_stay/test/F_Parhat_16_19/SignUpTest.java
@@ -1,16 +1,23 @@
 package com.four_stay.test.F_Parhat_16_19;
 
- import javax.swing.border.TitledBorder;
+ import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import javax.swing.border.TitledBorder;
 
 import org.openqa.selenium.By;
 
 //mvn -Dtest=FunctionalTest test 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
+import com.four_stay.pages.F_Parhay_16_19.HomePage;
 import com.four_stay.pages.F_Parhay_16_19.SignUpPage;
+import com.four_stay.utilities.BrowserUtils;
 import com.four_stay.utilities.ConfigurationReader;
 import com.four_stay.utilities.Driver;
 import com.four_stay.utilities.TestBase;
@@ -20,18 +27,39 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class SignUpTest extends TestBase{
 		
 	
-
-		
+	 HomePage homepage = new HomePage();
+	 SignUpPage signUp = new SignUpPage();
 	
-	 @Test(priority = 0, description = "test case TC016")
-	 public void test() {
-		String url= driver.getCurrentUrl();
-		System.out.println(url);
-		
-		//	 SignUpPage signUp = new SignUpPage();
-		 driver.get(ConfigurationReader.getProperty("signupurl"));
-		//signUp.login.click();
-	//	System.out.println(driver.getTitle());
-		
+	 @Test(priority = 0, description = "test case TC018")
+	 public void testCase18() {
+		assertTrue(homepage.isAt());
+		assertTrue(homepage.isAtUrl());
+		homepage.clickSearchBox();
+		assertTrue(homepage.searchBox.getText().isEmpty());
+		homepage.ClickSearchButton();
+		assertEquals(homepage.AfterclicktheButton.getText(), "Please type your city, college, or metro");
 	}
+	 
+	 
+	 
+	 @Test(priority = 1, description = "test case TC019")
+	 public void testCase19() {
+		homepage.sendKeyToSearchBox();
+		Select select = new Select((WebElement) driver);
+		select.getFirstSelectedOption();
+		homepage.ClickSearchButton();
+		
+	} 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 }
