0e37638307ab82df8ee0574bf63ef76ebcd5b042
rodriguez <rodriguez@10.0.0.48>
2018-03-17 00:51:00 -0400

    Rodriguez_Beta_4Stay Test 7 done

--- automation-test-4stay/src/test/java/com/four_stay/pages/C_Alex_7_9/HomePage.java
+++ automation-test-4stay/src/test/java/com/four_stay/pages/C_Alex_7_9/HomePage.java
@@ -1,24 +1,97 @@
 package com.four_stay.pages.C_Alex_7_9;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.four_stay.utilities.Driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-
+/**
+ * 
+ * @author Rodriguez HomeTest Beta_4Stay
+ *
+ */
 
 public class HomePage {
-	
+
 	private WebDriver driver;
-	
+
 	public HomePage() {
-		this.driver=Driver.getDriver();
+		this.driver = Driver.getDriver();
 		PageFactory.initElements(driver, this);
 	}
-	
-	
-	//	@FindBy(className="billLabel")
-	//	public WebElement billLabel;
+
+	@FindBy(xpath = "//a[contains(text(),'Log In')]")
+	public WebElement logIn;
+
+	@FindBy(xpath = "//input[@id='user_email']")
+	public WebElement emailAddress;
+
+	@FindBy(xpath = "//input[@id='user_password']")
+	public WebElement password;
+
+	@FindBy(xpath = "//a[@id='login_btn']")
+	public WebElement loginButton;
+
+	@FindBy(xpath = "//a[@class='forgot-link']")
+	public WebElement forgotPassword;
+
+	@FindBy(xpath = "//h2[contains(text(),'Forgot your password?')]")
+	public WebElement forgotPasswordMessageDisplay;
+
+	@FindBy(xpath = "//input[@type='submit']")
+	public WebElement resetInstruction;
+
+	@FindBy(xpath = "//div[@id='error_explanation']/ul/li")
+	public WebElement emailCantBeBlank;
+
+	public String verifyHomePageTitle() {
+		return driver.getTitle();
+	}
+
+	public boolean validateLogIn() {
+		return logIn.isDisplayed();
+	}
+
+	public boolean forgotPasswordDisplay() {
+		return forgotPasswordMessageDisplay.isDisplayed();
+	}
+
+	public void logInClickable() {
+		try {
+			logIn.click();
+			System.out.println("Log In is clickable");
+
+		} catch (NoSuchElementException e) {
+			System.out.println("Log in is not clickable");
+		}
+
+	}
+
+	public void emailAddressIsEmpty() {
+		try {
+			emailAddress.getText().isEmpty();
+			System.out.println("Email Address field is empty");
+
+		} catch (NoSuchElementException e) {
+			System.out.println("Email Address field is not empty");
+		}
+
+	}
+
+	public void passwordIsEmpty() {
+		try {
+			password.getText().isEmpty();
+			System.out.println("Password field is empty");
+
+		} catch (NoSuchElementException e) {
+			System.out.println("Password Field is not empty");
+		}
+
+	}
+
 }
--- automation-test-4stay/src/test/java/com/four_stay/test/C_Alex_7_9/FunctionalTest.java
+++ automation-test-4stay/src/test/java/com/four_stay/test/C_Alex_7_9/FunctionalTest.java
@@ -1,6 +1,9 @@
 package com.four_stay.test.C_Alex_7_9;
 
- import org.openqa.selenium.By;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
 
 //mvn -Dtest=FunctionalTest test 
 
@@ -8,22 +11,82 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
+import com.four_stay.pages.C_Alex_7_9.HomePage;
 import com.four_stay.utilities.TestBase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class FunctionalTest extends TestBase{
-		
+/**
+ * 
+ * @author Rodriguez HomeTest Beta_4Stay
+ *
+ */
+public class FunctionalTest extends TestBase {
 
+	/*
+	 * Step 1 Launch the "4stay.com" application with URL. https://4stay.com/ *Home
+	 * page is loeaded. *URL of leaded page is equal to ecpected URL.
+	 * https://4stay.com
+	 * 
+	 * Step 2 This step will test log in functionality/option Log in option on
+	 * homepage is displayed
+	 * 
+	 * Step 3 Log in option should be clickable, and new window for log in should be
+	 * displayed New window for log in should pop up.
+	 * 
+	 * Step 4 Email and Password fields should be available and no input should be
+	 * provided User field is empty Username: n/a
+	 * 
+	 * Step 5 User id field (email address) should be editable and no input should
+	 * be provided email address field is empty Userpassword: n/a
+	 * 
+	 * Step 6 "Forgot Password?" option is available "Forgot Password?" is displayed
+	 * 
+	 * Step 7 After clicking, "Forgot password?" New window is opened, and
+	 * "Forgot your password?" is displayed.
+	 * 
+	 * Step 8 Email field is left blank, and click
+	 * "Send me reset password instructions" option. The color verification will be
+	 * done manually. "Email can't be blank" is displayed in red color.
+	 */
 
-	 
-	// HomePage homepage = new HomePage();
-	 @Test(priority = 0, description = "First test case TC001")
-	 public void test() {
-		 System.out.println("hi");
-		
-			
-		
+	String actualUrl = "https://4stay.com/";
+	String actualForgotPassword ="Forgot Password?";
+	
+	HomePage homepage = new HomePage();
+
+	@Test(priority = 0, description = "First test case TC001")
+	public void test() throws InterruptedException {
+		//Step1
+		String expectedUrl = driver.getCurrentUrl();
+		assertEquals(expectedUrl, actualUrl);
+		//Step2
+		assertTrue(homepage.validateLogIn());
+		//Step3
+		homepage.logInClickable();
+		//Step4
+		boolean emptyEmail= homepage.emailAddress.getText().isEmpty();
+		assertTrue(emptyEmail);
+		//homepage.emailAddressIsEmpty();
+		//Step5
+		boolean emptyPassword=homepage.password.getText().isEmpty();
+		assertTrue(emptyPassword);
+		//homepage.passwordIsEmpty();
+		//Step6
+		String expectedForgotPassword = homepage.forgotPassword.getText();
+		assertEquals(expectedForgotPassword, actualForgotPassword);
+		//Step7
+		Thread.sleep(5000);
+		homepage.forgotPassword.click();
+		homepage.forgotPasswordDisplay();
+		//Step8
+		Thread.sleep(5000);
+		homepage.resetInstruction.click();
+		Thread.sleep(5000);
+		homepage.emailCantBeBlank.isDisplayed();
+		System.out.println(homepage.emailCantBeBlank.getText());
+		//done
 	}
+		
+	
 }
